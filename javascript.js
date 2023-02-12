3f9ab2b7932303394a80fa08565f838ac2d1bf78
Se-French <116462983+Se-French@users.noreply.github.com>
2023-02-12 14:11:46 -0500

    Create basic functioning of calculator

--- javascript.js
+++ javascript.js
@@ -17,16 +17,16 @@ function divide(a, b){
 
 function operate(operator, num1, num2){
     switch (operator){
-        case '+':
+        case 'add':
             return add(num1, num2);
             break;
-        case '-':
+        case 'subtract':
             return subtract(num1, num2);
             break;
-        case '*':
+        case 'multiply':
             return multiply(num1, num2);
             break;
-        case '/':
+        case 'divide':
             return divide(num1, num2);
             break;
         default:
@@ -42,3 +42,28 @@ numberButtons.forEach(button => button.addEventListener('click', getNumber));
 function getNumber(event){
     displayValue.textContent += event.target.id;
 }
+
+//This makes the calculator work
+const operations = document.querySelectorAll('.operators');
+operations.forEach(operation => operation.addEventListener('click', createOperation));
+
+function createOperation(event){
+    let operator = event.target.id;
+    let num1 = +(displayValue.textContent);
+    displayValue.textContent = '';
+    //console.log(operator);
+    //console.log(num1);
+    const equals = document.getElementById('equals');
+    equals.addEventListener('click', getResult);
+
+    function getResult(event){
+        let num2 = +(displayValue.textContent);
+        //console.log(num2);
+        //console.log(operate(operator, num1, num2));
+        let solution = (operate(operator, num1, num2));
+        displayValue.textContent = solution;
+    }
+}
+
+
+
